2457ad416c19167cf53fcfa37e48e312aef9c384
meamarp <appotdar05@gmail.com>
2022-04-24 19:27:58 +0530

    remove unwanted packege

--- app/main.py
+++ app/main.py
@@ -1,21 +1,21 @@
-from importlib.resources import path
 import pathlib
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 
-BASE_DIR =  pathlib.Path(__file__).parent
+BASE_DIR = pathlib.Path(__file__).parent
 
 app = FastAPI()
-#app/templates
+# app/templates
 templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
+
 @app.get("/", response_class=HTMLResponse)
-def home_view(request:Request):
-    return templates.TemplateResponse("home.html",{"request": request})
+def home_view(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
 
 
 @app.post("/")
 def home_detailed_view():
-    return {"hello":"master"}
+    return {"hello": "master"}
